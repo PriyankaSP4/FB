363e035c258ce373c60d132ea64ad3f7c2c56ba2
Priyanka Patel <priyanka.patel@softwire.com>
2020-07-06 16:36:10 +0100

    added comments to make rules clearer to part2

--- app/src/main/java/com/example/fizzbuzz/Part 2.kt
+++ app/src/main/java/com/example/fizzbuzz/Part 2.kt
@@ -1,27 +1,34 @@
 package com.example.fizzbuzz
 
 fun main() {
+
     for (x in 1..200) {
         var word: String = ""
+        // adds Fizz for 3
         if (x%3 == 0) {
             word += "Fizz"
             }
+        // adds Fezz for 13, after 3 but before everything else
         if (x%13 == 0) {
             word = "Fezz"
         }
+        // adds Buzz for 5
         if (x%5 == 0) {
             word += "Buzz"
         }
+        // adds Bang for 7
         if (x%7 == 0) {
             word += "Bang"
         }
+        // changes to only Bong for 11, or adds Bong if 13 also
         if (x%11 == 0) {
-            if (x%13 ==0) {
-                word += "Bong"
+            if (x%13 == 0) {
+                word = "FezzBong"
             } else {
                 word = "Bong"
             }
         }
+        // reverses for 17
         if (x%17 == 0) {
             var chunks = word.chunked(4)
             chunks = chunks.asReversed()
