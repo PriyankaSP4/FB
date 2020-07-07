ea7014c7df7869e2e26e64518348597be295e01b
Priyanka Patel <priyanka.patel@softwire.com>
2020-07-07 09:36:51 +0100

    added stretch goals, two separate programs

--- app/src/main/java/com/example/fizzbuzz/Stretch Goals.kt
+++ app/src/main/java/com/example/fizzbuzz/Stretch Goals.kt
@@ -1,5 +1,6 @@
 package com.example.fizzbuzz
 
+/*
 fun main() {
     // Get max output
     print("Up to what number do you want to print output?: ")
@@ -10,7 +11,7 @@ fun main() {
     rules.add(rule1)
     var more: String? = "Y"
     while (more == "Y") {
-        print("Would you like to implement another rule (Y/N)? ")
+        print("Would you like to implement another set rule (Y/N)? ")
         more = readLine()
         if (more == "Y") {
             print("Which rule would you like to implement? Enter the associated number: ")
@@ -23,7 +24,7 @@ fun main() {
     for (x in 1..max) {
         var word: String = ""
         // adds Fizz for 3
-        if (rules.contains("3") && x%3 == 0) {
+        if (rules.contains("3") && x % 3 == 0) {
             word += "Fizz"
         }
         // adds Fezz for 13, after 3 but before everything else
@@ -31,7 +32,7 @@ fun main() {
             word = "Fezz"
         }
         // adds Buzz for 5
-        if (rules.contains("5") && x%5 == 0) {
+        if (rules.contains("5") && x % 5 == 0) {
             word += "Buzz"
         }
         // adds Bang for 7
@@ -39,15 +40,15 @@ fun main() {
             word += "Bang"
         }
         // changes to only Bong for 11, or adds Bong if 13 also
-        if (rules.contains("11") && x%11 == 0) {
-            if (rules.contains("13") && x%13 == 0) {
+        if (rules.contains("11") && x % 11 == 0) {
+            if (rules.contains("13") && x % 13 == 0) {
                 word = "FezzBong"
             } else {
                 word = "Bong"
             }
         }
         // reverses for 17
-        if (rules.contains("17") && x%17 == 0) {
+        if (rules.contains("17") && x % 17 == 0) {
             var chunks = word.chunked(4)
             chunks = chunks.asReversed()
             word = ""
@@ -61,5 +62,39 @@ fun main() {
             print("$word")
         }
         print (" ")
+    }
+}
+*/
+
+fun main() {
+    // Get max output
+    print("Up to what number do you want to print output?: ")
+    val max = Integer.valueOf(readLine())
+
+    // Find out what rules the user wants to implement
+    print("How many rules would you like to implement? ")
+    val tot = Integer.valueOf(readLine())
+    var divs = mutableListOf<Int>()
+    var phrases = mutableListOf<String?>()
+    for (x in 1..tot) {
+        print("Rule $x - what number would you like to use as the divisor? ")
+        divs.add(Integer.valueOf(readLine()))
+        print("What four letter word would you like to print if the number is divisible by this number? ")
+        phrases.add(readLine())
+    }
+
+    //Generate output
+    for (num in 1..max) {
+        var word = ""
+        for (index in phrases.indices) {
+            if (num % divs[index] == 0) {
+                word += phrases[index]
+            }
+        }
+        if (word == "") {
+            print("$num ")
+        } else {
+            print("$word ")
+        }
     }
 }
