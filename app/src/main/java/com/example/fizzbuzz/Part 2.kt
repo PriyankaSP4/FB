19acfdce67a27ebf0c477146eba9e8f77e891ff3
Priyanka Patel <priyanka.patel@softwire.com>
2020-07-07 13:50:19 +0100

    tried to use functions for part 2?

--- app/src/main/java/com/example/fizzbuzz/Part 2.kt
+++ app/src/main/java/com/example/fizzbuzz/Part 2.kt
@@ -1,30 +1,27 @@
 package com.example.fizzbuzz
 
+import android.provider.UserDictionary.Words.addWord
+
 fun main() {
     // Generate output
-    for (x in 1..200) {
+    fun addWord(w: String, l: MutableList<String>): Boolean = l.add(w)
+
+    for (x in 1..4) {
         var words = mutableListOf<String>()
         // adds Fizz for 3
-        if (x%3 == 0) {
-            words.add("Fizz")
-        }
+        if (x%3 == 0) addWord("Fizz", words)
         // adds Buzz for 5
-        if (x%5 == 0) {
-            words.add("Buzz")
-        }
+        if (x%5 == 0) addWord("Buzz", words)
         // adds Bang for 7
-        if (x%7 == 0) {
-            words.add("Bang")
-        }
+        if (x%7 == 0) addWord("Bang", words)
         // changes to only Bong for 11
         if (x%11 == 0) {
             words = mutableListOf("Bong")
         }
-
         // adds Fezz for 13, after F words but before B words
         if (x%13 == 0) {
-            var bWords = words.dropWhile { it.startsWith('F')}
-            var fWords = words.takeWhile {it.startsWith('F')}
+            var bWords = words.dropWhile { !it.startsWith('B')}
+            var fWords = words.takeWhile {!it.startsWith('B')}
             var fezz = mutableListOf<String>("Fezz")
             words = (fWords + fezz + bWords) as MutableList<String>
             }
